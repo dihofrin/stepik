4fa608b576478968453361ab19992a930fbc4683
Robert Ford <serhiopich@gmail.com>
2023-01-25 17:22:59 +0300

    section 10.4

---
+++ Python for Professionals/10/part 4/step 7.py
@@ -0,0 +1,28 @@
+"""
+https://stepik.org/lesson/669733/step/7?unit=667881
+
+Итератор Repeater
+Реализуйте класс Repeater, порождающий итераторы, конструктор которого принимает один аргумент:
+
+obj — произвольный объект
+Итератор класса Repeater должен бесконечно генерировать единственное значение — obj.
+"""
+
+class Repeater:
+
+    def __init__(self, obj):
+        self.obj = obj
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.obj
+
+
+
+geek = Repeater('geek')
+
+print(next(geek))
+print(next(geek))
+print(next(geek))
