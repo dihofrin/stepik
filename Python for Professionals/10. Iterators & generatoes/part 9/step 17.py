0bd9acb108d5a874489704ed0ba40fed26310a56
Robert Ford <serhiopich@gmail.com>
2023-02-15 17:50:40 +0300

    section 10.9

---
+++ Python for Professionals/10. Iterators & generatoes/part 9/step 17.py
@@ -0,0 +1,14 @@
+"""
+https://stepik.org/lesson/666563/step/17?unit=664567
+
+Реализуйте функцию first_largest(), которая принимает два аргумента в следующем порядке:
+
+iterable — итерируемый объект, элементами которого являются целые числа
+number — произвольное число
+Функция должна возвращать индекс первого элемента итерируемого объекта iterable, который больше number. Если таких элементов нет, функция должна вернуть число −1.
+"""
+from itertools import dropwhile
+def first_largest(iterable, number):
+    for index in dropwhile(lambda x: x[1] < number, enumerate(iterable)):
+        return index[0]
+    return -1
